024cfd1357a7befcac232c466f4e1b9f6cb99a63
Tadeusz Niemiec <tadeusz@applover.pl>
2021-03-26 11:34:04 +0100

    remove unecessary command

--- vim_install.sh
+++ vim_install.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 sudo apt install vim curl
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s .vimrc ~/.vimrc
